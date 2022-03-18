12906f9ad394df9cde4fb420262c344804028481
MUKUNDA <mukundakamatham@gmail.com>
2022-03-18 10:23:42 +0000

    up

--- src/containers/homeContainer/homeContainer.jsx
+++ src/containers/homeContainer/homeContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-//import "./homeContainer.scss"
+import "./homeContainer.scss"
 export const HomeCard=(datas)=>{
 let data={
     img:'https://cdn.getyourguide.com/img/mic/crop-tour-277-a161e9a60b0847f3.jpeg/wide_320_uncropped_q50.webp',
@@ -10,20 +10,32 @@ let data={
     stars:"4.9",
     reviews:"100",
     button:"Adventure",
-    buttonColour:'black',
+    buttonColour:'teal',
     skipTheline:false,
     price:"$234",
 }
 
     return (
         <div className="homeContainer">
-          <img src={data.img} alt=""/>
-          <h2>{data.header}</h2>
-          {data.pickup?<h6>Pickup available</h6>:null}
-          {data.duration.length>0?<h6>Estimated duration {data.duration}</h6>:null}
-          {data.skipTheline?<h6>skip the line available</h6>:null}
+         <div><img src={data.img} alt=""/></div> 
+         <div className="homecard">
+         <div><h3>{data.header}</h3></div>
+         {data.pickup?<p>Pickup available</p>:null}
+         {data.duration.length>0?<p>Estimated duration {data.duration}</p>:null}
+         {data.skipTheline?<p>skip the line available</p>:null}
           <button style={{backgroundColor:data.buttonColour}}>{data.button}</button>
-          <div className="Stars" style={{"--rating": 2.3}} aria-label="Rating of this product is 2.3 out of 5."></div> 
+          <div  className="last"> <div><span> <div className="Stars" style={{"--rating": data.stars}} aria-label="Rating of this product is 2.3 out of 5.">
+          </div> </span> <span>{data.stars}</span><span>({data.reviews}reviews)</span> </div>
+          <p>From {data.price} OnWards</p>
+          </div>
+          </div>
         </div>
     )
 }
+
+/* 
+          
+          
+        
+     
+          */
